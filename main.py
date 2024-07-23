4733ef0a7076badc88f80aab51448da09aeef3c0
CherSula <chers1303@gmail.com>
2024-07-23 20:38:45 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+devider_line = "+---+---+---+"
+first_line = ("| " + ' ' + " ") * 3 + "|"
+
+print(devider_line)
+print(first_line)
+print(devider_line)
+print(first_line)
+print(devider_line)
+print(first_line)
+print(devider_line)
