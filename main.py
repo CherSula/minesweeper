2db97cf5ce11086c5f073462edd70e82c2e856b9
CherSula <chers1303@gmail.com>
2024-07-23 22:22:16 +0400

    add func generate_field

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
-devider_line = "+---+---+---+"
-first_line = ("| " + ' ' + " ") * 3 + "|"
+def generate_field(width, height):
+    devider_line = "+---" * width + "+"
+    for _ in range(1, height+1):
+        print(devider_line)
+        first_line = ("| " + ' ' + " ") * width + "|"
+        print(first_line)
+    print(devider_line)
 
-print(devider_line)
-print(first_line)
-print(devider_line)
-print(first_line)
-print(devider_line)
-print(first_line)
-print(devider_line)
+
+generate_field(6, 3)
