f9c821b6341c4add241c8430870fd546e4bba094
CherSula <chers1303@gmail.com>
2024-07-29 15:42:39 +0400

    add generators of view for cell and view for field

--- main.py
+++ main.py
@@ -15,11 +15,103 @@ def create_line(row):
     return line
 
 
+def generate_field_data_view(field):
+    data_view = generate_empty_field(field)
+    for row_ind in range(len(field)):
+        for col_ind in range(len(field[row_ind])):
+            data_view[row_ind][col_ind] = generate_view_for_cell(
+                field, row_ind, col_ind)
+    return data_view
+
+
+def generate_empty_field(field):
+    height = len(field)
+    width = len(field[0])
+    empty_field = []
+    for _ in range(height):
+        el = []
+        empty_field.append(el)
+        for _ in range(width):
+            el.append('')
+    return empty_field
+
+
+def generate_view_for_cell(field, r_ind, c_ind):
+    value = field[r_ind][c_ind]
+    if value == 1:
+        return '*'
+
+    counter = 0
+    left_col_index = c_ind - 1
+    right_col_index = c_ind + 1
+    up_row_index = r_ind - 1
+    down_row_index = r_ind + 1
+
+    if left_col_index >= 0 and field[r_ind][left_col_index] == 1:
+        counter += 1
+    if (
+        right_col_index < len(field[r_ind])
+        and field[r_ind][right_col_index] == 1
+    ):
+        counter += 1
+    if (
+        left_col_index >= 0
+        and down_row_index < len(field)
+        and field[down_row_index][left_col_index] == 1
+    ):
+        counter += 1
+
+    if down_row_index < len(field) and field[down_row_index][c_ind] == 1:
+        counter += 1
+    if (
+        down_row_index < len(field)
+        and right_col_index < len(field[r_ind])
+        and field[down_row_index][right_col_index] == 1
+    ):
+        counter += 1
+
+    if (
+        left_col_index >= 0
+        and up_row_index >= 0
+        and field[up_row_index][left_col_index] == 1
+    ):
+        counter += 1
+
+    if (
+        up_row_index >= 0
+        and field[up_row_index][c_ind] == 1
+    ):
+        counter += 1
+    if (
+        up_row_index >= 0
+        and right_col_index < len(field[r_ind])
+        and field[up_row_index][right_col_index] == 1
+    ):
+        counter += 1
+
+    return str(counter)
+
+
 field = [
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]-
+    ]
+
+empty_field_data_view = [
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', '']
+    ]
+
+field_data_view = [
     ['1', '*', '2', '1'],
     ['1', '2', '*', '1'],
     ['0', '1', '1', '1'],
     ['0', '0', '0', '0']
     ]
 
-draw_field(field)
+field_data_view = generate_field_data_view(field)
+draw_field(field_data_view)
